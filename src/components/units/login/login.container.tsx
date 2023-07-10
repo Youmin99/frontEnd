3bd31b4f8e69d86bd858af00a0d80e8c51f1ec5d
youmin <105027461+futureyp@users.noreply.github.com>
2023-07-09 23:14:59 -0700

    login page

--- src/components/units/login/login.container.tsx
+++ src/components/units/login/login.container.tsx
@@ -1,27 +1,87 @@
-import { useState } from "react";
-import * as S from "./login.styles";
-// import { Modal } from 'antd';
+import { ChangeEvent, useState } from 'react';
+import * as S from'./login.styles';
+import {Button, Modal ,Rate} from 'antd';
+import Link from 'next/link';
 
-export default function Login(): JSX.Element {
-  const [isTrue, setIsTrue] = useState(false);
+export default function Login() {
+	const [isTrue, setIsTrue] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
-  const handleModal = (): void => {
-    setIsTrue((prev) => !prev);
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>):void => {
+    setEmail(event.target.value);
+    if (!email.includes("@")) {
+      setEmailError("이메일 주소를 다시 확인해주세요.");
+    } else {
+      setEmailError("");
+    }
   };
 
-  return (
-    <>
-      <S.Wrapper>
-        <button onClick={handleModal}>나와라 모달</button>
-        {isTrue && (
-          <S.ModalWrapper>
-            <S.Modal>
-              <S.Title>log in</S.Title>
-              <button onClick={handleModal}>모달 닫기</button>
-            </S.Modal>
-          </S.ModalWrapper>
-        )}
-      </S.Wrapper>
-    </>
-  );
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>):void => {
+    setPassword(event.target.value);
+    if (password.length >= 8 && password.length <= 16) {
+      setPasswordError("");
+    } else {
+      setPasswordError("8~16자의 영문,숫자,특수 문자만 사용 가능합니다.");
+    }
+  };
+  const onClickLogin = () => {
+    if (emailError === "" && passwordError === "") {
+      alert("환영합니다");
+    } 
+  };
+
+
+	const handleModal = () => {
+		setIsTrue((prev) => !prev);
+	};
+
+	return (
+		<>
+			<S.Wrapper>
+			    
+			{!isTrue && (<button onClick={handleModal}>나와라 모달</button>)}
+				{isTrue && (
+					<S.ModalWrapper>
+						<S.Modal>
+						  <S.Title>login</S.Title>
+						
+						  <S.Wapper>
+						  <S.EmailInputWrapper>
+						    <S.EmailInput 
+							  type="email"
+							  placeholder="email"
+                              onChange={onChangeEmail}>
+							</S.EmailInput>
+							<S.EmailError>{emailError}</S.EmailError>
+						  </S.EmailInputWrapper>
+						  <S.PasswordInputWrapper>
+						    <S.PasswordInput 
+							  type="password"
+							  placeholder="password"
+                              onChange={onChangePassword}>
+							</S.PasswordInput>
+							<S.PasswordError>{passwordError}</S.PasswordError>
+						  </S.PasswordInputWrapper>
+						  <S.buttonWrapper>
+						    <S.submit onChange={onClickLogin}>log in</S.submit>
+						  </S.buttonWrapper>
+						  </S.Wapper>
+						  <S.EtcButton>
+						    <S.linkStyle href="/about">find email</S.linkStyle>
+						    <S.EtcItem>|</S.EtcItem>
+						    <S.linkStyle href="/about">find password</S.linkStyle>
+							<S.EtcItem>|</S.EtcItem>
+						    <S.linkStyle href="/about">create acount</S.linkStyle>
+						  </S.EtcButton>
+						 
+
+						</S.Modal>
+					</S.ModalWrapper>
+				)}
+			</S.Wrapper>
+		</>
+	);
 }
--- src/components/units/login/login.styles.ts
+++ src/components/units/login/login.styles.ts
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import Link from 'next/link';
 
 export const Wrapper = styled.div`
 	width: 100vw;
@@ -15,22 +16,149 @@ export const ModalWrapper = styled.div`
 	width: 100vw;
 	height: 100vh;
 	background: black;
-	opacity: 0.7;
+	opacity: 0.5;
 `;
 
 export const Modal  = styled.div`
 	position: absolute;
-	display: flex;
-	justify-content: center;
-	align-items: center;
 	background-color: white;
+
+	justify-content: center;
+	
 	top: 100px;
-	width: 500px;
+	width: 400px;
 	height: 500px;
 	border: 1px solid black;
 	border-radius: 20px;
 	opacity: 1;
 `;
 
-export const Title  = styled.div`
+export const Title  = styled.h1`
+    height: 100px;
+    padding: 20px 0px 0px 20px;
 `;
+
+export const submit  = styled.button`
+    height: 40px;
+	width: 300px;
+	background-color: #ff7f00;
+	border-radius: 30px;
+	border:none;
+`;
+
+export const buttonWrapper = styled.div`
+  padding:10px;
+  display:flex;
+`;
+
+export const createAccount  = styled.span`
+    height: 40px;
+	width: 300px;
+	background-color: #ff7f00;
+	border-radius: 30px;
+	border:none;
+`;
+
+export const linkStyle = styled(Link)`
+  font-size: 50px;
+  color: black;
+  cursor: pointer;
+`;
+
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 100px;
+`;
+export const Wapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+`;
+export const LogoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 220px;
+  padding-bottom: 160px;
+`;
+
+export const LogoTitle = styled.div`
+  font-size: 60px;
+  font-weight: bold;
+  color: #fff;
+  padding-top: 30px;
+`;
+export const InputWrapper = styled.div`
+  width: 540px;
+`;
+export const EmailInputWrapper = styled.div`
+  border-bottom: 1px solid #fff;
+  padding-bottom: 20px;
+  margin-bottom: 10px;
+`;
+export const EmailInput = styled.input`
+height: 40px;
+width: 300px;
+  font-size: 16px;
+  background-color: #bebebe;
+  border: none;
+  background: none;
+  outline: none;
+  border-radius: 30px;
+`;
+export const PasswordInputWrapper = styled.div`
+  border-bottom: 1px solid #fff;
+  padding-bottom: 20px;
+  margin-bottom: 10px;
+`;
+export const PasswordInput = styled.input`
+  height: 40px;
+  width: 300px;
+  margin-top: 40px;
+  font-size: 16px;
+  background: #bebebe;
+  background-color:red;
+  border: none;
+  background: none;
+  outline: none;
+  border-radius: 30px;
+`;
+export const EmailError = styled.div`
+  padding-top: 5px;
+  font-size: 10px;
+  color: #ff1b6d;
+`;
+export const PasswordError = styled.div`
+  padding-top: 5px;
+  font-size: 10px;
+  color: #ff1b6d;
+`;
+export const LoginButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 76px;
+  border-radius: 38px;
+  padding: 25px 0;
+  background-color: rgba(255, 27, 109, 0.6);
+  margin: 20px 0 45px 0;
+  cursor: pointer;
+`;
+
+export const EtcButton = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+`;
+export const EtcItem = styled.div`
+  font-size: 20px;
+  color: black;
+  cursor: pointer;
+`;
+
+
+
