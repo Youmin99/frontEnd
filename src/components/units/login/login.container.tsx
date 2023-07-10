7694094775c668a6ef7eb742a4a1362025756502
youmin <105027461+futureyp@users.noreply.github.com>
2023-07-09 23:25:16 -0700

    check

--- src/components/units/login/login.container.tsx
+++ src/components/units/login/login.container.tsx
@@ -1,16 +1,15 @@
-import { ChangeEvent, useState } from 'react';
-import * as S from'./login.styles';
-import {Button, Modal ,Rate} from 'antd';
-import Link from 'next/link';
+import type { ChangeEvent } from "react";
+import { useState } from "react";
+import * as S from "./login.styles";
 
-export default function Login() {
-	const [isTrue, setIsTrue] = useState(false);
+export default function Login(): JSX.Element {
+  const [isTrue, setIsTrue] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
 
-  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>):void => {
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>): void => {
     setEmail(event.target.value);
     if (!email.includes("@")) {
       setEmailError("이메일 주소를 다시 확인해주세요.");
@@ -19,7 +18,7 @@ export default function Login() {
     }
   };
 
-  const onChangePassword = (event: ChangeEvent<HTMLInputElement>):void => {
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>): void => {
     setPassword(event.target.value);
     if (password.length >= 8 && password.length <= 16) {
       setPasswordError("");
@@ -27,61 +26,57 @@ export default function Login() {
       setPasswordError("8~16자의 영문,숫자,특수 문자만 사용 가능합니다.");
     }
   };
-  const onClickLogin = () => {
+  const onClickLogin = (): void => {
     if (emailError === "" && passwordError === "") {
       alert("환영합니다");
-    } 
+    }
   };
 
+  const handleModal = (): void => {
+    setIsTrue((prev) => !prev);
+  };
 
-	const handleModal = () => {
-		setIsTrue((prev) => !prev);
-	};
+  return (
+    <>
+      <S.Wrapper>
+        {!isTrue && <button onClick={handleModal}>나와라 모달</button>}
+        {isTrue && (
+          <S.ModalWrapper>
+            <S.Modal>
+              <S.Title>login</S.Title>
 
-	return (
-		<>
-			<S.Wrapper>
-			    
-			{!isTrue && (<button onClick={handleModal}>나와라 모달</button>)}
-				{isTrue && (
-					<S.ModalWrapper>
-						<S.Modal>
-						  <S.Title>login</S.Title>
-						
-						  <S.Wapper>
-						  <S.EmailInputWrapper>
-						    <S.EmailInput 
-							  type="email"
-							  placeholder="email"
-                              onChange={onChangeEmail}>
-							</S.EmailInput>
-							<S.EmailError>{emailError}</S.EmailError>
-						  </S.EmailInputWrapper>
-						  <S.PasswordInputWrapper>
-						    <S.PasswordInput 
-							  type="password"
-							  placeholder="password"
-                              onChange={onChangePassword}>
-							</S.PasswordInput>
-							<S.PasswordError>{passwordError}</S.PasswordError>
-						  </S.PasswordInputWrapper>
-						  <S.buttonWrapper>
-						    <S.submit onChange={onClickLogin}>log in</S.submit>
-						  </S.buttonWrapper>
-						  </S.Wapper>
-						  <S.EtcButton>
-						    <S.linkStyle href="/about">find email</S.linkStyle>
-						    <S.EtcItem>|</S.EtcItem>
-						    <S.linkStyle href="/about">find password</S.linkStyle>
-							<S.EtcItem>|</S.EtcItem>
-						    <S.linkStyle href="/about">create acount</S.linkStyle>
-						  </S.EtcButton>
-						 
-
-						</S.Modal>
-					</S.ModalWrapper>
-				)}
-			</S.Wrapper>
-		</>
-	);
+              <S.Wapper>
+                <S.EmailInputWrapper>
+                  <S.EmailInput
+                    type="email"
+                    placeholder="email"
+                    onChange={onChangeEmail}
+                  ></S.EmailInput>
+                  <S.EmailError>{emailError}</S.EmailError>
+                </S.EmailInputWrapper>
+                <S.PasswordInputWrapper>
+                  <S.PasswordInput
+                    type="password"
+                    placeholder="password"
+                    onChange={onChangePassword}
+                  ></S.PasswordInput>
+                  <S.PasswordError>{passwordError}</S.PasswordError>
+                </S.PasswordInputWrapper>
+                <S.buttonWrapper>
+                  <S.submit onChange={onClickLogin}>log in</S.submit>
+                </S.buttonWrapper>
+              </S.Wapper>
+              <S.EtcButton>
+                <S.linkStyle href="/about">find email</S.linkStyle>
+                <S.EtcItem>|</S.EtcItem>
+                <S.linkStyle href="/about">find password</S.linkStyle>
+                <S.EtcItem>|</S.EtcItem>
+                <S.linkStyle href="/about">create acount</S.linkStyle>
+              </S.EtcButton>
+            </S.Modal>
+          </S.ModalWrapper>
+        )}
+      </S.Wrapper>
+    </>
+  );
 }
