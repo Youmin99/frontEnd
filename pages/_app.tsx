7dece25de256152b78eb18cb318f8115a4e28a68
youmin <logic9904@gmail.com>
2023-12-11 14:17:48 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,17 @@ import ApolloSetting from "../src/components/commons/apollo";
 import Layout from "../src/components/commons/layout";
 import { RecoilRoot } from "recoil";
 import React from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <RecoilRoot>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <ApolloSetting>
         <React.Fragment>
           <Global styles={globalStyles} />
